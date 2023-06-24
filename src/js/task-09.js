6b3953b0d6658094807d31ea95508130eb6b83bd
Olga-Olga <olga_l@ukr.net>
2023-06-24 19:01:17 +0300

    123

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+   // .padStart(6, 0)
+    }`;
+
 }
+
+const bodyElement = document.body
+const btnElement = document.querySelector(".change-color")
+const spanElement = document.querySelector(".color")
+
+btnElement.addEventListener("click", ({ target }) => {
+  bodyElement.style.backgroundColor = getRandomHexColor();
+spanElement.textContent = getRandomHexColor()
+  console.log(bodyElement);
+})
