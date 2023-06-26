ccc153c9d949be3f8bd41089132390ddd33e7016
Olga-Olga <olga_l@ukr.net>
2023-06-26 18:08:08 +0300

    fixed

--- js/task-10.js
+++ js/task-10.js
@@ -17,41 +17,44 @@ btnElementDestroy.removeEventListener("click", handlerButtonDraw);
 
 btnElementDestroy.addEventListener("click", destroyBoxes);
 
+boxElement.style.display = "flex";
+boxElement.style.flexDirection = "column";
+boxElement.style.alignItems = "center";
+  
 let quantityValue = 0;
+
 function handlerButtonDraw() {
   if (!inputElement.value) {
     alert("Enter valid number")
     return;
   }
-
-  if (inputElement.value > 100 || inputElement.value < 0) {
-    alert("Enter < 100")
-    return;
-  }
-
-  quantityValue = Number(inputElement.value);
+  // if (inputElement.value > 100 || inputElement.value < 0) {
+  //   alert("Enter < 100")
+  //   return;
+  // }
+  quantityValue += Number(inputElement.value);
   createBoxes(quantityValue)
 }
 
+console.log(quantityValue);
+
 function createBoxes(param) {
   const collectionDivElements = [];
-  for (let i = 0; i < param; i += 1) {
+  for (let i = 0; i < (param); i += 1) {
+    boxElement.innerHTML = '';
     const x = document.createElement("div")
     x.style.width = `${30 + i * 10}px`;
     x.style.height = `${30 + i * 10}px`;
-    x.textContent = i + 1;
+    x.textContent = param + 1;
     x.style.textAlign = "center";
     x.style.color = "white"
     x.style.backgroundColor = getRandomHexColor();
     collectionDivElements.push(x);
-      console.log(x);
+    console.log(x);
   }
-  boxElement.innerHTML = '';
-  boxElement.style.display = "flex";
-  boxElement.style.flexDirection = "column";
-  boxElement.style.alignItems = "center";
   console.log("boxElement");
   boxElement.append(...collectionDivElements)
+  
   }
 
 function destroyBoxes() {
